ed3d71d5ff928def803230a874e17ba8ca1d2680
Cody <cody@hastings.email>
2017-11-26 10:51:03 -0600

    setting up basic logic for calculating input carbs, proteins, fats % of total

--- public/main.js
+++ public/main.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
 
-  
+  $("#submit").click(getInputs);
+
+  function getInputs(){
+    var inputCalories = $("#calories").val();
+    var inputCarbs = $("#carbs").val();
+    var inputProteins = $("#proteins").val();
+    var inputFats = $("#fats").val();
+    
+    var carbPercent = calculateInputs(inputCalories, inputCarbs);
+    var proteinPercent = calculateInputs(inputCalories, inputProteins);
+    var fatsPercent = calculateInputs(inputCalories, inputFats);
+
+    console.log(carbPercent + " " + proteinPercent + " " + fatsPercent);
+  }
+
+  function calculateInputs(total, value){
+
+    return value / total;
+
+  }
+
 });
